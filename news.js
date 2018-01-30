6130f01f8d2df6f6c804c502a0bfe09ef02a7b3b
Xiaoyi Wang <xiaoyi.wang.2016@gmail.com>
2018-01-29 22:05:31 -0600

    css

--- news.js
+++ news.js
@@ -1,11 +1,13 @@
 // navbar.style.position = "fixed";
 // navbar.style.bottom = '0';
 
+
+var mykey = //YOUR_KEY_HERE
 var xhr = new XMLHttpRequest();
 
 var url = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
-          'apiKey=a4747a4cffac44e9a8ffd0e16c69db7b';
+          'apiKey='+mykey;
 
 if (document.getElementById('news_reader')) {
 	if (document.getElementById('news_reader').style.visibility == 'visible')
@@ -22,15 +24,7 @@ if (document.getElementById('news_reader')) {
 }
 
 else {
-	console.log('here3')
-
 	var div = document.createElement('div');
-	div.style.height = "50px";
-	div.style.width = "100%"
-	div.style.background = "grey";
-	div.style.position = 'fixed';
-	div.style.bottom = '0px';
-	div.style.zIndex = '100';
 	div.id = 'news_reader';
 	div.className = "news_reader";
 	div.style.visibility = "visible"
@@ -44,27 +38,7 @@ else {
 	document.getElementById('news_reader').appendChild(scroll);
 }
 
-
-xhr.open("GET", url, true);
-xhr.onreadystatechange = function() {
-	if (xhr.readyState == 4) {
-		var resp = JSON.parse(xhr.responseText);
-		var articles = resp.articles;
-		var output = "";
-		for (a=0; a<articles.length-1; a++)
-		{
-			output += "<a href="+articles[a].url+'style="color: rgb(255,255,255)" target="_blank">'+articles[a].title+"</a>"
-			output += '&nbsp &nbsp &nbsp &nbsp &#8226 &nbsp &nbsp &nbsp &nbsp';
-		}
-		output += "<a href="+articles[a].url+'style="color: rgb(255,255,255)" target="_blank">'+articles[a].title+"</a>"
-		document.getElementById('scroll').innerHTML = output
-	}
-}
-xhr.send();
-
-
-document.getElementById('scroll').addEventListener("animationend", listener, false)
-function listener(e) {
+function get_news() {
 	xhr.open("GET", url, true);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
@@ -73,10 +47,11 @@ function listener(e) {
 			var output = "";
 			for (a=0; a<articles.length-1; a++)
 			{
-				output += "<a href="+articles[a].url+'style="color: rgb(255,255,255)" target="_blank">'+articles[a].title+"</a>"
+				output += "<a href="+articles[a].url+' style="color: rgb(255,255,255)" target="_blank">'+articles[a].title+"</a>"
 				output += '&nbsp &nbsp &nbsp &nbsp &#8226 &nbsp &nbsp &nbsp &nbsp';
 			}
-			output += "<a href="+articles[a].url+'style="color: rgb(255,255,255)" target="_blank">'+articles[a].title+"</a>"
+			output += "<a href="+articles[articles.length-1].url+"style='color: rgb(255,255,255)' target='_blank'>"+articles[articles.length-1].title+"</a>"
+			document.getElementById('scroll').innerHTML = output
 			var newElem = document.getElementById('scroll');
 			var replaced = newElem.cloneNode(true);
 			newElem.parentNode.replaceChild(replaced, newElem);
@@ -86,6 +61,13 @@ function listener(e) {
 		}
 	}
 	xhr.send();
+
+}
+
+get_news()
+
+function listener(e) {
+	get_news()
 	
 }
 
